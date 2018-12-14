a4e5a58a902cd473b46cb5e1418625797edb94aa
Clement Bourdarie <clement.bourdarie@imerir.com>
2018-12-14 21:24:33 +0200

    login progress bar

--- src/monoko/ui/LoginBase.java
+++ src/monoko/ui/LoginBase.java
@@ -3,16 +3,25 @@ package monoko.ui;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 public abstract class LoginBase implements Initializable{
+	@FXML HBox _contentHBox;
+	
 	@FXML TextField _idTextfield;
 	@FXML PasswordField _passwordTextfield;
 	
 	@FXML Button _loginButton;
 	@FXML Button _registerButton;
 	@FXML Button _quitButton;
+	
+	@FXML ProgressIndicator _loginProgressIndicator;
+	
+	@FXML Label _loginLabel;
 	
 	@FXML public abstract void onLoginClicked();
 	@FXML public abstract void onRegisterClicked();
--- src/monoko/ui/LoginController.java
+++ src/monoko/ui/LoginController.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 
 import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
 import javafx.stage.Stage;
 import monoko.objects.Player;
 import monoko.utils.FxmlManager;
@@ -25,28 +26,52 @@ public class LoginController extends LoginBase{
 
 	@Override
 	public void onLoginClicked() {
-		try {
-			
-			if(_idTextfield.getText().isEmpty() || _passwordTextfield.getText().isEmpty()) {
-				Stage dialog = new Stage();
-				Scene dialogScene = new Scene(new FxmlManager("./ui/warning.fxml", new WarningController("Please fill the ID and Password fields.")).load(), 400, 100);
-				dialog.setTitle("Warning");
-                dialog.setScene(dialogScene);
-                dialog.show();
-			}else {
-				Player player = new Network().login(_idTextfield.getText(), _passwordTextfield.getText()); 
-				if( player != null ) {
-					_parent.getRootAnchorPane().getChildren().set(0, new FxmlManager("./ui/mainMenu.fxml", new MainMenuController(this.getParent())).load());
-				}
-			}
-			
-		} catch (Exception e) {
+		if(_idTextfield.getText().isEmpty() || _passwordTextfield.getText().isEmpty()) {
 			Stage dialog = new Stage();
-			Scene dialogScene = new Scene(new FxmlManager("./ui/warning.fxml", new WarningController("Login error: Check your ID and password.")).load(), 400, 100);
-			dialog.setTitle("Login Error");
+			Scene dialogScene = new Scene(new FxmlManager("./ui/warning.fxml", new WarningController("Please fill the ID and Password fields.")).load(), 400, 100);
+			dialog.setTitle("Warning");
             dialog.setScene(dialogScene);
             dialog.show();
+		}else {
 			
+			new Thread(new Runnable() {
+			    @Override public void run() {
+		        Platform.runLater(new Runnable() {
+		            @Override public void run() {
+		            	_loginProgressIndicator.setVisible(true);
+						_loginLabel.setVisible(true);
+		                _loginProgressIndicator.setProgress(ProgressBar.INDETERMINATE_PROGRESS);
+		                
+						try {
+							Player player = new Network().login(_idTextfield.getText(), _passwordTextfield.getText());
+							if( player != null ) {
+								_parent.getRootAnchorPane().getChildren().set(0, new FxmlManager("./ui/mainMenu.fxml", new MainMenuController(_parent)).load());
+								_loginProgressIndicator.setVisible(false);
+								_loginLabel.setVisible(false);
+							}
+						} catch (Exception e) {
+							
+							new Thread(new Runnable() {
+							    @Override public void run() {
+						        Platform.runLater(new Runnable() {
+						            @Override public void run() {
+										_loginProgressIndicator.setVisible(false);
+										_loginLabel.setVisible(false);
+										
+										Stage dialog = new Stage();
+										Scene dialogScene = new Scene(new FxmlManager("./ui/warning.fxml", new WarningController("Login error: Check your ID and password.")).load(), 400, 100);
+										dialog.setTitle("Login Error");
+							            dialog.setScene(dialogScene);
+							            dialog.show();
+						            }
+						        });
+							}}).start();
+
+						} 
+		            }
+		        });
+			   }
+			}).start();
 		}
 	}
 
