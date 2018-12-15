7bb13e850eac61b85ae4ab168b40b85bcb88ba92
Clement Bourdarie <clement.bourdarie@imerir.com>
2018-12-15 15:49:17 +0200

    .

---
+++ src/monoko/ui/TeamEditorBase.java
@@ -0,0 +1,19 @@
+package monoko.ui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
+
+public abstract class TeamEditorBase implements Initializable{
+	@FXML FlowPane _charactersFlowpane;
+	
+	@FXML VBox _teamsVBox;
+
+	@FXML Button _createCharacterButton;
+	@FXML Button _createTeamButton;
+	
+	@FXML public abstract void onCreateCharacterClicked();
+	@FXML public abstract void onCreateTeamClicked();
+}
---
+++ src/monoko/ui/TeamEditorController.java
@@ -0,0 +1,29 @@
+package monoko.ui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.scene.layout.AnchorPane;
+import monoko.utils.FxmlManager;
+
+public class TeamEditorController extends TeamEditorBase{
+
+	public TeamEditorController() {}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle bundle) {
+
+	}
+
+	@Override
+	public void onCreateCharacterClicked() {
+		
+	}
+
+	@Override
+	public void onCreateTeamClicked() {
+		
+	}
+
+	
+}
