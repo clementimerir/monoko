93637b19f7f2fc61455ba91b5957732fc04c199b
Clement Bourdarie <clement.bourdarie@imerir.com>
2018-12-15 17:24:48 +0200

    arrete de casser le code david stp

--- src/monoko/ui/CharacterBase.java
+++ src/monoko/ui/CharacterBase.java
@@ -3,10 +3,13 @@ package monoko.ui;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 public abstract class CharacterBase implements Initializable{
 	@FXML VBox _teamsVBox;
+	
+	@FXML Label _nameLabel;
 
 	@FXML Button _editButton;
 	@FXML Button _deleteButton;
